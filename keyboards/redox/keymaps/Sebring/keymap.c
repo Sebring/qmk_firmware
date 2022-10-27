a7361c6414a7b9b575da821f8f2f94d73058e2c7
J.Sebring <johan.sebring@regionstockholm.se>
2022-10-27 14:31:59 +0200

    navigate with F

--- keyboards/redox/keymaps/Sebring/keymap.c
+++ keyboards/redox/keymaps/Sebring/keymap.c
@@ -27,7 +27,6 @@ enum custom_keycodes {
 
 #define KC_NAGR LT(_NAV, KC_GRV)
 #define KC_NAMI LT(_NAV, KC_MINS)
-
 #define KC_ADEN LT(_ADJUST, KC_END)
 #define KC_ADPU LT(_ADJUST, KC_PGUP)
 
@@ -41,6 +40,7 @@ enum custom_keycodes {
 #define LSYM_KCM LT(_SYMB, KC_M)
 #define LNAV_KCT LT(_NAV, KC_T)
 #define LNAV_KCN LT(_NAV, KC_N)
+#define LNAV_F LT(_NAV, KC_F)
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_ESC  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,RALT(KC_7),                        RALT(KC_0), KC_Y ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_LBRC ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB  ,KC_A    ,KC_S    ,KC_D,    KC_F    , LSYM_KCG   ,RSFT(KC_8),                    RSFT(KC_9)  ,KC_H    , KC_J   , KC_K   , KC_L   , KC_SCLN,KC_QUOT ,
+     KC_TAB  ,KC_A    ,KC_S    ,KC_D,    LNAV_F    , LSYM_KCG   ,RSFT(KC_8),                    RSFT(KC_9)  ,KC_H    , KC_J   , KC_K   , KC_L   , KC_SCLN,KC_QUOT ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_ADPU ,KC_PGDN ,        KC_HOME ,KC_ADEN ,KC_N    , KC_M   ,KC_COMM , KC_DOT , KC_UP  , KC_RSFT ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
@@ -87,11 +87,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______ ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,                                            KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,_______ ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-  _______ ,KC_EXLM ,RALT(KC_2),KC_LCBR ,KC_RCBR ,RSFT(KC_2) , RALT(KC_8),                     RALT(KC_9) ,KC_PSLS ,KC_P7   ,KC_P8   ,KC_P9   ,KC_PMNS ,XXXXXXX ,
+  _______ ,KC_EXLM ,RALT(KC_2),KC_LCBR ,KC_RCBR ,RSFT(KC_2) , RALT(KC_8),                     RALT(KC_9) ,KC_PSLS ,KC_P7   ,KC_P8, KC_P9, KC_PMNS ,  KC_F11 ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______ ,KC_HASH ,KC_DLR  ,KC_LBRC ,KC_RBRC , KC_CTPL, _______,                      _______ ,KC_PAST ,KC_P4   ,KC_P5   ,KC_P6   ,KC_PPLS ,XXXXXXX ,
+     _______ ,KC_HASH ,KC_DLR  ,KC_LBRC ,KC_RBRC , KC_CTPL, _______,                         _______ , KC_PAST,  KC_P4,  KC_P5,   KC_P6,   KC_PPLS,  KC_F12 ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______ ,KC_PERC ,KC_CIRC ,KC_LPRN ,KC_RPRN , BCKTCK , RSFT(KC_1),RSFT(KC_MINS) ,     KC_SLSH, RSFT(KC_SLSH), XXXXXXX ,KC_P1   ,KC_P2   ,KC_P3   ,KC_PENT ,XXXXXXX ,
+     _______ ,KC_PERC ,KC_CIRC ,KC_LPRN ,KC_RPRN , BCKTCK , RSFT(KC_1),RSFT(KC_MINS) ,KC_SLSH, RSFT(KC_SLSH), XXXXXXX ,KC_P1   ,KC_P2   ,KC_P3   ,KC_PENT ,XXXXXXX ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
      _______ ,_______ ,_______ ,_______ ,     _______ ,    _______ ,_______ ,         _______, _______,   KC_P0,       KC_P0   ,KC_PDOT ,KC_PENT ,XXXXXXX 
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
