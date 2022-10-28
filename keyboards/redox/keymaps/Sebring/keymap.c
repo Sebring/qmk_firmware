bb42e9ca9ffe95a56ff4c57aa682437a819f350f
J.Sebring <johan.sebring@regionstockholm.se>
2022-10-28 12:57:35 +0200

    nav with J

--- keyboards/redox/keymaps/Sebring/keymap.c
+++ keyboards/redox/keymaps/Sebring/keymap.c
@@ -41,6 +41,7 @@ enum custom_keycodes {
 #define LNAV_KCT LT(_NAV, KC_T)
 #define LNAV_KCN LT(_NAV, KC_N)
 #define LNAV_F LT(_NAV, KC_F)
+#define LNAV_J LT(_NAV, KC_J)
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
@@ -61,11 +62,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_ESC  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,RALT(KC_7),                        RALT(KC_0), KC_Y ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_LBRC ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB  ,KC_A    ,KC_S    ,KC_D,    LNAV_F    , LSYM_KCG   ,RSFT(KC_8),                    RSFT(KC_9)  ,KC_H    , KC_J   , KC_K   , KC_L   , KC_SCLN,KC_QUOT ,
+     KC_TAB  ,KC_A    ,KC_S    ,KC_D,    LNAV_F  , LSYM_KCG ,S(KC_8),                          S(KC_9),  KC_H,  LNAV_J,    KC_K   , KC_L   , KC_SCLN,KC_QUOT ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_ADPU ,KC_PGDN ,        KC_HOME ,KC_ADEN ,KC_N    , KC_M   ,KC_COMM , KC_DOT , KC_UP  , KC_RSFT ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     KC_LCTRL ,KC_LGUI ,KC_PMNS ,KC_ALAS ,     KC_CTPL ,    KC_BSPC ,KC_DEL  ,        KC_ENT  ,KC_SPC  ,    KC_RALT,     XXXXXXX , KC_LEFT, KC_DOWN, KC_RGHT 
+     KC_LCTRL ,KC_LGUI ,KC_LALT, KC_ALAS ,     KC_CTPL ,    KC_BSPC ,KC_DEL  ,        KC_ENT  ,KC_SPC  ,    KC_RALT,     XXXXXXX , KC_LEFT, KC_DOWN, KC_RGHT 
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
